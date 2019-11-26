d08b73fc935ec65af2dcf4700519fd3bd3fe3832
liuyongfu <liuyongfu@saiyaoyun.com>
2019-11-26 11:10:56 +0800

    完善侧边栏折叠显示

--- src/pages/home/subNav.js
+++ src/pages/home/subNav.js
@@ -2,29 +2,29 @@ import React, { useState } from "react";
 import style from "./home.less";
 import classnames from "classnames";
 import ReactSVG from "react-svg";
-import {Icon} from 'antd'
-import {useStoreState} from 'easy-peasy'
+import { Icon, Tooltip, Popover } from "antd";
+import { useStoreState } from "easy-peasy";
 import { useHistory, useLocation } from "react-router-dom";
 
 function SubNav() {
   let history = useHistory();
   const location = useLocation();
-  const isOpenSidebar=useStoreState(state=>state.layout.isOpenSidebar)
+  const isOpenSidebar = useStoreState(state => state.layout.isOpenSidebar);
   const [currentClickNav, setCurrentClickNav] = useState({});
   const baseNavList = [
     { text: "图表展示", roles: ["user", "admin"], icon: "saleTab", path: "/chart" },
     { text: "设备管理", roles: ["user", "admin"], icon: "machine", path: "/operator" },
     { text: "订单管理", roles: ["admin"], icon: "order", path: "/device" },
-      {
-        text: "商品管理",
-        roles: ["user", "admin"],
-        icon: "shop",
-        key: "shop",
-        children: [
-          { text: "地方", roles: ["user", "admin"], icon: "order", path: "/abc" },
-          { text: "哈佛", roles: ["admin"], icon: "account", path: "/bbc" }
-        ]
-      },
+    {
+      text: "商品管理",
+      roles: ["user", "admin"],
+      icon: "shop",
+      key: "shop",
+      children: [
+        { text: "地方", roles: ["user", "admin"], icon: "order", path: "/abc" },
+        { text: "哈佛", roles: ["admin"], icon: "account", path: "/bbc" }
+      ]
+    },
     { text: "账目管理", roles: ["admin"], icon: "account", path: "/account" },
     {
       text: "会员管理",
@@ -71,7 +71,7 @@ function SubNav() {
   filterData(cloneData);
   const navList = cloneData;
 
-  // 目录点击后
+  // 菜单项点击后
   function onListClick(item) {
     if (item.children) {
       setCurrentClickNav({ ...currentClickNav, [item.key]: !currentClickNav[item.key] });
@@ -80,16 +80,58 @@ function SubNav() {
     }
   }
 
+  // 折叠下hover的菜单项
+  function popEle(data) {
+    return data.map(item => (
+      <div key={item.text}>
+        {!item.children && (
+          <li
+            className={classnames({ popItem: true, popActive: location.pathname.startsWith(item.path) })}
+            onClick={() => onListClick(item)}
+          >
+            {item.text}
+          </li>
+        )}
+        {/* 显示子菜单 */}
+        {item.children && (
+          <Popover content={popEle(item.children)} placement="right" overlayClassName="subNavPop">
+            <li
+              className={classnames({ popItem: true, popActive: location.pathname.startsWith(item.path) })}
+              onClick={() => onListClick(item)}
+            >
+              {item.text}
+            </li>
+          </Popover>
+        )}
+      </div>
+    ));
+  }
+
+  // 侧边栏菜单
   function listEle(data) {
     return data.map(item => (
       <div key={item.text}>
         {/* 各菜单项 */}
         <li onClick={() => onListClick(item)}>
-          <div className={classnames({ listItem: true, navActive: location.pathname.startsWith(item.path )})}>
-            <ReactSVG className="inlineSVG" src={require(`@/assets/${item.icon}.svg`)} />
-            {isOpenSidebar&&<span className="text">{item.text}</span>}
+          <div className={classnames({ listItem: true, navActive: location.pathname.startsWith(item.path) })}>
+            {/* 折叠显示图标 */}
+            {isOpenSidebar && <ReactSVG className="inlineSVG" src={require(`@/assets/${item.icon}.svg`)} />}
+            {/* 折叠有子菜单显示pop */}
+            {!isOpenSidebar && item.children && (
+              <Popover content={popEle(item.children)}  placement="right" overlayClassName="navPop">
+                <ReactSVG className="inlineSVG" src={require(`@/assets/${item.icon}.svg`)} />
+              </Popover>
+            )}
+            {/* 折叠没子菜单显示tip */}
+            {!isOpenSidebar && !item.children && (
+              <Tooltip title={item.text} placement="right" overlayClassName="navTooltip">
+                <ReactSVG className="inlineSVG" src={require(`@/assets/${item.icon}.svg`)} />
+              </Tooltip>
+            )}
+            {/* 非折叠显示文本 */}
+            {isOpenSidebar && <span className="text">{item.text}</span>}
             {/* 折叠菜单icon */}
-            {item.children &&isOpenSidebar&& (
+            {item.children && isOpenSidebar && (
               <ReactSVG
                 className={classnames({ inlineSVG: true, "icon-open": currentClickNav[item.key] })}
                 src={require("@/assets/right.svg")}
@@ -98,9 +140,10 @@ function SubNav() {
           </div>
         </li>
         {/* 递归嵌套菜单 */}
-        {item.children && (
+        {item.children && isOpenSidebar && (
           <div
             className={classnames({
+              subMenu: true,
               "subList-hidden": !currentClickNav[item.key]
             })}
           >
@@ -112,21 +155,16 @@ function SubNav() {
   }
 
   return (
-    <div className={classnames({[style.sidebarList]:true,[style.closeSidebar]:!isOpenSidebar})}>
+    <div className={classnames({ [style.sidebarList]: true, [style.closeSidebar]: !isOpenSidebar })}>
       {/* 头部logo */}
       <div className="header-logo" onClick={() => history.push("/home")}>
-        <Icon  type='github'/>
-        {isOpenSidebar&&<span className="text">管理系统</span>}
+        <Icon type="github" />
+        {isOpenSidebar && <span className="text">管理系统</span>}
       </div>
       {/* menu项 */}
-      <ul className='listWrap'>
-      {listEle(navList)}
-      </ul>
-
+      <ul className="listWrap">{listEle(navList)}</ul>
     </div>
   );
 }
 
 export default SubNav;
-
-
